3e04f5654a416507deee49b8e702fd4053919762
Abdallah-Moh <abdallah.mam2007@gmail.com>
2021-06-13 12:10:29 +0200

    :arrow_up: Update Rhyme Icons Version

--- electron.js
+++ electron.js
@@ -1,27 +1,45 @@
 const { BrowserWindow, Menu, app } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
+const htmlFile = path.join(__dirname, "public", "index.html");
 
 if (isDev) {
-  require("electron-reload")(__dirname, {
+  require("electron-reload")(path.join(__dirname, "public"), {
     electron: require(`${__dirname}/node_modules/electron`),
   });
 }
-// Disable hardware acceleration and enabling transparent visuals
-app.disableHardwareAcceleration();
-app.commandLine.appendSwitch("enable-transparent-visuals");
 
 app.on("ready", () => {
-  setTimeout(() => {
-    let win = new BrowserWindow({
-      icon: path.join(__dirname, "icon.png"),
-      title: "Rhyme",
-      width: 900,
-      height: 600,
-      frame: process.platform === "linux" ? true : false,
-      titleBarStyle: "hiddenInset",
-      transparent: true,
-    });
-    win.loadFile(path.join(__dirname, "public", "index.html"));
-  }, 500);
+  let win = new BrowserWindow({
+    icon: path.join(__dirname, "icon.png"),
+    title: "Rhyme",
+    width: 900,
+    height: 600,
+    webPreferences: {
+      webSecurity: true,
+      contextIsolation: false,
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  });
+
+  win.loadFile(htmlFile);
+  if (isDev) {
+    win.setMenu(
+      Menu.buildFromTemplate([
+        {
+          label: "Toggle Dev Tools",
+          click() {
+            win.webContents.toggleDevTools();
+          },
+        },
+        {
+          label: "Force Reload",
+          click() {
+            win.loadFile(htmlFile);
+          },
+        },
+      ])
+    );
+  }
 });
---
+++ src/store.ts
@@ -0,0 +1,4 @@
+import { writable } from "svelte/store";
+const storage = require("electron-json-storage");
+
+export const settings = writable<object>(storage.getSync("settings"));
