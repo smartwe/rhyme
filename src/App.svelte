586f90c08b14857d01b249702518a8a1294779ef
Abdallah-Moh <abdallah.mam2007@gmail.com>
2021-06-13 13:02:42 +0200

    Bump `rhyme-icons` to `v1.1.3`

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+  import { Add, HomeOutlined, HomeFilled } from "rhyme-icons";
 </script>
 
-<main />
+<main>
+  <Add size="32px" fill="black" />
+  <HomeOutlined size="32px" fill="black" />
+  <HomeFilled size="32px" fill="black" />
+</main>
 
 <style>
   main {
     width: 100vw;
     height: 100vh;
-    background-color: #424242;
-    border-radius: 7px;
   }
 </style>
