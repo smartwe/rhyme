8b60742e9a8615f7e40f3419814ed82b9b3eb83d
abdallah-moh <abdallah.mam29@gmail.com>
2022-01-28 20:48:38 +0200

    :fire: Remove evrything

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,35 +1,15 @@
 import path from 'path';
-import { BrowserWindow, Notification as ElectronNotification, app, ipcMain, dialog, screen } from 'electron';
+import { BrowserWindow, app } from 'electron';
 import Store from 'electron-store';
 
 Store.initRenderer();
 
 import * as ModulesManager from './modules-manager';
-import GlobalShortcutsModule from './modules/global-shortcuts';
-import StorageManagerModule from './modules/managers/storage-manager';
-import TrayModule from './modules/tray';
-import WindowManagerModule from './modules/managers/window-manager';
-import IpcManagerModule from './modules/managers/ipc-manager';
-import SingleInstanceModule from './modules/single-instance-module';
 
 let window: Electron.BrowserWindow;
-const store = new Store();
 __dirname = app.getAppPath();
 
 app.on('ready', () => {
-  const currentDisplayBounds = screen.getPrimaryDisplay().bounds;
-  const bounds: { width: number; x: number; y: number; height: number } = (store.get('window-bounds') as {
-    width: number;
-    x: number;
-    y: number;
-    height: number;
-  }) ?? {
-    width: currentDisplayBounds.width / 2 > 800 ? currentDisplayBounds.width / 2 : 800, // 800 is the minimum width of the window
-    x: currentDisplayBounds.x,
-    y: currentDisplayBounds.y,
-    height: currentDisplayBounds.height / 2 > 400 ? currentDisplayBounds.height / 2 : 400, // 400 is the minimum width of the window
-  };
-
   window = new BrowserWindow({
     icon:
       process.platform === 'win32'
@@ -38,10 +18,10 @@ app.on('ready', () => {
     title: 'Rhyme',
     minWidth: 800,
     minHeight: 400,
-    width: bounds.width,
-    height: bounds.height,
-    x: bounds.x,
-    y: bounds.y,
+    // width: bounds.width,
+    // height: bounds.height,
+    // x: bounds.x,
+    // y: bounds.y,
     webPreferences: {
       webSecurity: true,
       contextIsolation: false,
@@ -50,28 +30,10 @@ app.on('ready', () => {
     },
   });
 
-  ipcMain.on('notification', (_event, body) => {
-    new ElectronNotification({ title: 'Rhyme', body }).show();
-  });
+  window.loadURL(`file://${path.join(__dirname, 'dist', 'renderer', 'index.html')}`);
+  window.setMenu(null);
 
-  ipcMain.handle('show-dialog', async (_event, config) => {
-    const data = await dialog.showOpenDialog(window, {
-      properties: [config.dialogType],
-      title: config.title,
-      defaultPath: config.defaultPath,
-    });
-
-    return data.filePaths[0];
-  });
-
-  ModulesManager.init(
-    new WindowManagerModule(__dirname, window),
-    new IpcManagerModule(__dirname, window),
-    new GlobalShortcutsModule(__dirname, window),
-    new StorageManagerModule(__dirname),
-    new SingleInstanceModule(__dirname, window),
-    new TrayModule(__dirname, window)
-  ).catch((error: Error) => {
+  ModulesManager.init().catch((error: Error) => {
     if (error) throw error;
   });
 });
--- src/renderer/App.svelte
+++ src/renderer/App.svelte
@@ -1,52 +1,9 @@
-<script lang="ts">
-  import HomeView from './views/HomeView.svelte';
-  import Router from 'svelte-spa-router';
-  import { currentTheme } from './services/store';
-  import SideBar from './components/widgets/SideBar.svelte';
-  import StatusBar from './components/widgets/StatusBar.svelte';
-
-  import Scrollbar from 'smooth-scrollbar';
-  import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
-  import BottomBar from './components/widgets/BottomBar.svelte';
-  Scrollbar.use(OverscrollPlugin);
-
-  const routes = {
-    '/': HomeView,
-  };
+<script>
+  // your script goes here
 </script>
 
-<main
-  style="
-  --background-color:{$currentTheme.colors.backgroundColor};
-  --panels-color:{$currentTheme.colors.panelsColor};
-  --title-color:{$currentTheme.colors.titleColor};
-  --accent-color:{$currentTheme.colors.accentColor};
-  --sidebar-active-color:{$currentTheme.colors.sidebarActiveColor};
-  --text-color:{$currentTheme.colors.textColor};"
->
-  <div class="main-content">
-    <SideBar />
-    <div class="router">
-      <Router {routes} />
-      <BottomBar />
-    </div>
-  </div>
-  <StatusBar />
-</main>
+<main>Rhyme is cool</main>
 
-<style lang="scss">
-  main {
-    width: 100vw;
-    height: 100vh;
-  }
-  .main-content {
-    width: 100%;
-    height: calc(100% - 24px);
-    display: flex;
-  }
-  .router {
-    width: calc(100% - 230px);
-    display: flex;
-    flex-direction: column;
-  }
+<style>
+  /* your styles go here */
 </style>
